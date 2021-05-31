54d5d0b71c81e45042719b417f40a93465fbd32b
Jesus Acedo <58938064+jacedoe@users.noreply.github.com>
2021-05-31 21:08:09 +0200

    Add files via upload

--- vue/guess_number/index.js
+++ vue/guess_number/index.js
@@ -4,36 +4,11 @@
 var app = new Vue({
   el: '#app',
   data() {
-
-    return {
-      message: 1,
-     secret: Math.floor(Math.random() * 100)
+      return {
+          message: 0,
+    // Creación del número aleatorio en el rango de 1 a 100 y redondeo a número entero
+     secret: Math.floor(Math.random() * 1 * 100)
     }
-  },
-
-  methods: {
-    endevina() {
-                
-                let x = parseInt(prompt("Introduce un número"));
-                
-                do {
-                    if (x < app.secret) {
-                        alert("El número secreto es superior");
-                        endevina();
-                    } else if (x > app.secret) {
-                        alert("El número secreto es inferior");
-                        endevina();
-                        
-                    } 
-            
-                    
-                }
-                 while (x !== app.secret);
-                
-                {
-                    alert("Has acertado, el número secreto es:" + " " + app.secret)
-                }
-            } 
   }
 
 })
